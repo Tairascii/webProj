a28c3ec2fe8f2b7dfe44074e2bc0641c5c3cb568
Tairascii <64748528+Tairascii@users.noreply.github.com>
2021-04-24 13:51:36 +0600

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import {Router} from '@angular/router';
+import {CartService} from './cart.service';
+import {Users} from './fake-db';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  static isLogged: boolean;
+  title = 'project';
+  constructor(public router: Router) {
+    AppComponent.isLogged = false;
+  }
+  get isLogged(): boolean{
+    return AppComponent.isLogged;
+  }
+  logout(): void{
+    for (const user of Users){
+      if (user.status){
+        user.status = false;
+      }
+    }
+    AppComponent.isLogged = false;
+  }
+}
