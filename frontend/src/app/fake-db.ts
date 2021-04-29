c0dfe339bed8da4bc9d4a63f36fd621c08ec14d1
Tairascii <64748528+Tairascii@users.noreply.github.com>
2021-04-29 17:57:53 +0600

    Delete frontend directory

--- frontend/src/app/fake-db.ts
+++
@@ -1,6 +0,0 @@
-import {User} from './models';
-
-export const Users: User[] = [
-  {id: 0, name: 'ascii', password: 'Aminalove2', status: false},
-  {id: 1, name: 'nino', password: '1231', status: false}
-];
